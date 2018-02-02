94d43fed672a2fa7199f0e06f7736fb4ccc3bcb1
Tomek Gryszkiewicz <grych@tg.pl>
2018-02-02 17:00:07 +0100

    0.7.0

--- mix.exs
+++ mix.exs
@@ -34,6 +34,6 @@ defmodule DrabExample.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:drab, "~> 0.4.1"}]
+     {:drab, "~> 0.7.0"}]
   end
 end
